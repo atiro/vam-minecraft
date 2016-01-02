bd78218771fdb8eda1c4d32ad121b5badb0b9fd8
Richard Palmer <git@tiro.org.uk>
2016-01-02 20:03:41 +0000

    Working json to schematic script

--- collections/json-to-nbt.py
+++ collections/json-to-nbt.py
@@ -6,28 +6,42 @@ from nbt import *
 
 # Export as NBT
 
-nbtfile = nbt.NBTFile()
+with open('bed-of-ware.json', 'r') as json_f:
+    schematic_j = json.load(json_f)
 
-nbtfile.name = "Schematic"
+    for object in schematic_j["schematics"]:
+        nbtfile = nbt.NBTFile()
+        nbtfile.name = "Schematic"
 
-nbtfile.tags.append(nbt.TAG_Short(name="Width", value=2))
-nbtfile.tags.append(nbt.TAG_Short(name="Length", value=2))
-nbtfile.tags.append(nbt.TAG_Short(name="Height", value=2))
-nbtfile.tags.append(nbt.TAG_String(name="Materials", value="Alpha"))
+        width = object["width"]
+        nbtfile.tags.append(nbt.TAG_Short(name="Width", value=width))
 
-blocks = nbt.TAG_Byte_Array(name="Blocks")
-blocks.value = bytearray([3,3,6,3,3,7,3,3])
-nbtfile.tags.append(blocks)
+        height = object["height"]
+        nbtfile.tags.append(nbt.TAG_Short(name="Height", value=height))
 
-data = nbt.TAG_Byte_Array(name="Data")
-data.value = bytearray([0,0,0,0,0,0,0,0])
-nbtfile.tags.append(data)
+        length = object["length"]
+        nbtfile.tags.append(nbt.TAG_Short(name="Length", value=length))
 
-entities = nbt.TAG_List(name="Entities", type=nbt.TAG_Compound)
-nbtfile.tags.append(entities)
+        materials = object["materials"]
+        nbtfile.tags.append(nbt.TAG_String(name="Materials", value=materials))
 
-tile_entities = nbt.TAG_List(name="TileEntities", type=nbt.TAG_Compound)
-nbtfile.tags.append(tile_entities)
+        blocks = nbt.TAG_Byte_Array(name="Blocks")
+        blocks_data = object["blocks"]
+        blocks.value = bytearray(blocks_data)
+        nbtfile.tags.append(blocks)
 
-print(nbtfile.pretty_tree())
-nbtfile.write_file("test.schematic")
+        data = nbt.TAG_Byte_Array(name="Data")
+        data_data = object["data"]
+        data.value = bytearray(data_data)
+        nbtfile.tags.append(data)
+
+        entities = nbt.TAG_List(name="Entities", type=nbt.TAG_Compound)
+        nbtfile.tags.append(entities)
+
+        tile_entities = nbt.TAG_List(
+            name="TileEntities", type=nbt.TAG_Compound)
+
+        nbtfile.tags.append(tile_entities)
+
+        print(nbtfile.pretty_tree())
+        nbtfile.write_file("test2.schematic")
